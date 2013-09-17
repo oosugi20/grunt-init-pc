0c0fa6140f5e4994f6fb535481cb2c1fa1f79ab7
Makoto OOSUGI <oosugi20@gmail.com>
2013-09-18 03:56:18 +0900

    Start project

---
+++ template.js
@@ -0,0 +1,37 @@
+/*
+ */
+
+'use strict';
+
+// Basic template description.
+exports.description = 'Create a basic project.';
+
+// Template-specific notes to be displayed before question prompts.
+exports.notes = '';
+
+// Template-specific notes to be displayed after question prompts.
+exports.after = '';
+
+// Any existing file or directory matching this wildcard will cause a warning.
+exports.warnOn = '*';
+
+// The actual init template.
+exports.template = function(grunt, init, done) {
+
+  init.process({}, [
+    // Prompt for these values.
+    init.prompt('project_name'),
+    init.prompt('author_name')
+  ], function(err, props) {
+
+    // Files to copy (and process).
+    var files = init.filesToCopy(props);
+
+    // Actually copy (and process) files.
+    init.copyAndProcess(files, props);
+
+    // All done!
+    done();
+  });
+
+};
